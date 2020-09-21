6e618fca7f6c878551c3b68615d217008f64fe96
Jen Speak <jspeak6@gmail.com>
2020-09-21 13:09:16 -0700

    added desktop styles

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const switchButton = document.querySelector('.header__switch input');
-const monthlyPrice = document.querySelectorAll('.card__price--month');
 const cards = document.querySelectorAll('.card');
-const yearlyPrice = document.querySelectorAll('.card__price--year');
 
 switchButton.addEventListener('change', e => {
     if (e.target.checked) {
